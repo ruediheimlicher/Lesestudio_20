b31b40b4c4ab82a278f9cad175e459c9ea9050b9
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2015-09-02 13:45:39 +0200

    first commit

---
+++ Lesestudio_20/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Lesestudio_20
+//
+//  Created by Ruedi Heimlicher on 01.09.2015.
+//  Copyright (c) 2015 Ruedi Heimlicher. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+   // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+   // Insert code here to tear down your application
+}
+
+@end
---
+++ Lesestudio_20/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lesestudio_20
+//
+//  Created by Ruedi Heimlicher on 01.09.2015.
+//  Copyright (c) 2015 Ruedi Heimlicher. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+   [super viewDidLoad];
+
+   // Do any additional setup after loading the view.
+}
+
+
+
+- (void)setRepresentedObject:(id)representedObject {
+   [super setRepresentedObject:representedObject];
+
+   // Update the view, if already loaded.
+}
+
+@end
